34d2e8cd87f2f0d97c11fa5b50f77f465408d745
knznmrcy <sakhaadya493@gmail.com>
2026-08-31 21:31:44 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,8 @@
 -- ==============================================
---     BLUEHAVEN HUB - PANDU HUB
+--     BLUEHAVEN HUB - Kantzy
 -- ==============================================
-
--- LOAD LIB
-local repo = "https://raw.githubusercontent.com/kezodxyz/KezodX/refs/heads/main/"
-local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
-local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
-local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
+SEMENTARA NO LIBRARY(KANTZY) 
+GABISA DI LOAD PUNG GAUSAH MALING
 
 local Options = Library.Options
 local Toggles = Library.Toggles
